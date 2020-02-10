295aaee8a61ee57a8d180ed108c7edcd656be7d4
Philipp Weinmann <philipp.weinmann71@gmail.com>
2020-02-10 15:55:14 +0100

    added wait on login page

--- iliasLogin.js
+++ iliasLogin.js
@@ -3,7 +3,8 @@
 if (window.location.href.indexOf("ilias.studium.kit.edu/login") > -1) {
   clickButtonById('f807');
 } else if (window.location.href.indexOf("idp.scc.kit.edu") > -1) {
-  clickButtonById('sbmt');
+  // add small timeout so that the browser has time to add in the passwords automatically. (time in milliseconds)
+  setTimeout(clickButtonById('sbmt'), 200);
 } else if (window.location.href.indexOf("campus.studium.kit.edu") > -1) {
   clickButtonByClass('shib-login shib-button');
 } else if (window.location.href.indexOf("ilias.studium.kit.edu/ilias.php?baseClass=ilrepositorygui") > -1) {
