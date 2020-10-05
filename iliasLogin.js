4a42932bc463d9133ec36245268fc5a668babba7
philippweinmann <philipp.weinmann71@gmail.com>
2020-10-05 12:57:41 +0200

    added logging

--- iliasLogin.js
+++ iliasLogin.js
@@ -1,21 +1,25 @@
-/// script to go through the ilias pages
+/// script to go through the ilias and campus pages
 
 if (window.location.href.indexOf("ilias.studium.kit.edu/login") > -1) {
+  console.log("URL detected");
   clickButtonById('f807');
-} else if (window.location.href.indexOf("idp.scc.kit.edu") > -1) {
-  // add small timeout so that the browser has time to add in the passwords automatically. (time in milliseconds)
-  setTimeout(clickButtonById('sbmt'), 200);
 } else if (window.location.href.indexOf("campus.studium.kit.edu") > -1) {
+  console.log("URL detected");
   clickButtonByClass('shib-login shib-button');
 } else if (window.location.href.indexOf("ilias.studium.kit.edu/ilias.php?baseClass=ilrepositorygui") > -1) {
+  console.log("URL detected");
   window.location.replace("https://ilias.studium.kit.edu/login.php?target=&client_id=produktiv&cmd=force_login&lang=de");
 }
 
 
 function clickButtonById(id) {
   document.getElementById(id).click();
+  console.log("button clicked");
 }
 
 function clickButtonByClass(className) {
+  console.log("clickButtonByClass is called with className = " + className);
+  console.log(getElementsByClassName(className)[0]);
   document.getElementsByClassName(className)[0].click();
+  console.log("button clicked");
 }
