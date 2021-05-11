ea7c01ccd758b4a62e74918b000bc8536c8baeed
Philipp Weinmann <philipp.weinmann71@gmail.com>
2021-05-11 14:45:24 +0200

    fixed bug with extension firering when clicking on a link to latest forum

--- iliasLogin.js
+++ iliasLogin.js
@@ -5,17 +5,34 @@ if (window.location.href.indexOf("ilias.studium.kit.edu/login") > -1) {
   clickButtonById('f807');
 } else if (window.location.href.indexOf("campus.studium.kit.edu") > -1) {
   console.log("URL detected");
-  setTimeout(function(){clickButtonByClass('shib-login shib-button');}, 100); //
+  setTimeout(function () { clickButtonByClass('shib-login shib-button'); }, 100); //
 } else if (window.location.href.indexOf("idp.scc.kit.edu") > -1) {
   // Careful, here chrome doesn't load the passwords in properly (Not exactly clear why)
   // Before clicking on submit, we will use a trick and click on some text beforehand.
-  clickButtonByClass('text full', 1); // only text, not an actual button. Index = 1 because with Index = 0 is a class with a button in it which we do not want to click on
+
+  // only text, not an actual button. Index = 1 because with Index = 0 
+  // is a class with a button in it which we do not want to click on
+  clickButtonByClass('text full', 1);
+  
   clickButtonById('sbmt'); // the actual button
-} else if ((window.location.href.indexOf("https://ilias.studium.kit.edu/ilias.php?baseClass=ilRepositoryGUI") > -1) | (window.location.href.indexOf("https://ilias.studium.kit.edu/ilias.php?baseClass=ilrepositorygui") > -1)) {
+} else if (detectMoreComplexUrl()) {
   console.log("URL detected");
   window.location.replace("https://ilias.studium.kit.edu/login.php?target=&client_id=produktiv&cmd=force_login&lang=de");
 }
 
+function detectMoreComplexUrl() {
+  // return false for an URL to a forum Link
+  if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/ilias.php?baseclass=ilrepositorygui&cmd=viewthread") > -1) {
+    console.log("Forum Link detected, IliasLogin not executing because you should already be logged in.")
+    return false
+  }
+
+  // return true for any other URL which links to Magazin but is not a forum URL. If you are already logged in, the login Button is not there so it won't fire
+  if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/ilias.php?baseclass=ilrepositorygui") > -1) {
+    console.log("Forum Link detected, IliasLogin not executing because you should already be logged in.")
+    return true
+  }
+}
 
 function clickButtonById(id) {
   document.getElementById(id).click();
@@ -23,8 +40,6 @@ function clickButtonById(id) {
 }
 
 function clickButtonByClass(className, index = 0) {
-  console.log("clickButtonByClass is called with className = " + className);
-  console.log("document state: " + document.readyState);
   var shibButton = document.getElementsByClassName(className)[index];
   console.log("button about to be clicked: " + shibButton);
   shibButton.click();
